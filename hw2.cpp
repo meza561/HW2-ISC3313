2e38bfdf154eac2f44a2d0bda6c65092768d0fee
meza561 <noahmeza561@gmail.com>
2026-09-10 18:08:42 -0400

    added total shopping time counter

--- hw2.cpp
+++ hw2.cpp
@@ -3,6 +3,7 @@ using namespace std;
 // main function
 int main() {
     int user_input;
+    int counter = 0;
 //gets user input
     cout << "Enter a positive integer: " << endl;
     cin >> user_input;
@@ -12,11 +13,16 @@ int main() {
         //even process
         if (user_input % 2 == 0)
             user_input = user_input / 2;
+
         //odd process
         else
             user_input = user_input * 3 + 1;
+
         cout << user_input << " ";
+        // adds a count for each operation
+        counter ++ ;
     }
+    cout << "\n Total shopping time: " << counter << endl;
     cout << endl;
     return 0;
 }
