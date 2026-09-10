66890389765dc7bd40d574ad5db644b910967ce0
meza561 <noahmeza561@gmail.com>
2026-09-10 16:06:34 -0400

    commenting

--- hw2.cpp
+++ hw2.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
-
+// main function
 int main() {
     int user_input;
+//gets user input
     cout << "Enter a positive integer: " << endl;
     cin >> user_input;
-
     cout << user_input << " ";
+    //stops when it hits 1
     while (user_input != 1) {
+        //even process
         if (user_input % 2 == 0)
             user_input = user_input / 2;
+        //odd process
         else
             user_input = user_input * 3 + 1;
         cout << user_input << " ";
